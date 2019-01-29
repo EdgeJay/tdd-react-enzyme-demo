06d26ff20cf0f7e1e2ce359570161cbbb81f484b
Wu Huijie <hjwu85@gmail.com>
2019-01-29 16:57:37 +0800

    more ui components to add-on to base

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from './Header';
 import Section from './Section';
+import SectionTitle from './SectionTitle';
 
 const Container = styled.div`
   padding: 2rem;
@@ -11,7 +12,9 @@ function App() {
   return (
     <Container>
       <Header />
-      <Section />
+      <Section>
+        <SectionTitle>Vote for your favourite framework!</SectionTitle>
+      </Section>
     </Container>
   );
 }
